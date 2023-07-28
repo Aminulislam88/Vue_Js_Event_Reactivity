3a86c584eeadcc250d4899c2478d72e82c0a0b6b
Aminul Islam <aislam181043@bscse.uiu.ac.bd>
2023-07-29 01:59:13 +0600

    episode 8 click event,keyup event

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,70 @@
 <script setup>
 import {ref,reactive} from 'vue'
-const fullImage=ref("https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60")
-const images=[
-    {
-        thumb:"https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
-        full:"https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60"
-    },
-    {
-        thumb:"https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=300&q=60",
-        full:"https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=300&q=60"
-    },
-    {
-        thumb:"https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
-        full:"https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60"
-    },
-    {
-        thumb:"https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
-        full:"https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60"
-    }
-]
+const message = ref("Hello Vue 3")
 
-function setImage(picture){
-    fullImage.value=picture
-    // fullImage.value=image.full
+const space = ref(0)
+
+function spaceClicked(){
+    space.value++
+    message.value="you pressed space " + space.value + " times"
 }
+
+function pressedWhat(event){
+  message.value="you pressed " +event.key
+}
+
+function presssedButton(){
+  message.value="you clicked the button"
+}
+
+function ctlRightButton(){
+  message.value="you clicked the ctrl button"
+}
+
+function divClicked(){
+  alert("clicked the div")
+}
+
+function buttonClicked(){
+  alert("clicked the button")
+}
+
 
 </script>
 
 <template>
  <section class="container mx-auto flex items-center flex-col">
-        <h1 class="text-center text-2xl py-10">Events & Reactivity</h1>
-        <div class="">
-            <div class="flex space-x-2" >
-                <img @click="setImage(image.full)" class="cursor-pointer" v-for="image in images" :key="image.thumb" :src="image.thumb" alt="">
-                <!-- @click= setImage(image) -->
-            </div>
-            <div class="mt-10">
-                <img class="cursor-pointer w-full" :src="fullImage" alt="">
-            </div>
-        </div>
-    </section>
+    <h1 class="text-center text-2xl py-10">Events & Reactivity in Vue.js</h1>
+    <h2 class="mt-10">{{ message }}</h2>
+    <form @submit.prevent action="https://google.com/search">
+      <div class="container mx-auto flex space-x-5 justify-center m-5">
+        <input @keyup.space="spaceClicked()"  name="q" type="text"  class="border border-gray-500 bg-white p-5"  placeholder="Press Space">
+      </div>
+      
+      <div class="container mx-auto flex space-x-5 justify-center m-5">
+        <input @keyup="pressedWhat($event)" name="text" type="text" class="border border-gray-500 bg-white p-5"  placeholder="Type Something">
+      </div>
+
+      <button @click.prevent="presssedButton()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >
+        Button
+      </button>
+
+      <button @click.ctrl="ctlRightButton()" class="bg-blue-500 ml-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >
+        Ctrl + Right Click
+      </button>
+
+    </form>
+
+    <div @click="divClicked()" class="bg-gray-200 w-[500px] h-80 mt-5 flex items-center justify-center"  >
+      <button @click="buttonClicked()" class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"  >
+        Button
+      </button>
+    </div>
+  </section>
 </template>
 
 <style scoped>
 
 </style>
+
+
